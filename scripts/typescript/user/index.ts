df0d138dfbf6b15f42c61628d4e804c611bb0205
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-09-29 08:52:28 +0100

    working on the user page

---
+++ scripts/typescript/user/index.ts
@@ -0,0 +1,5 @@
+function login() {
+    let username = (document.getElementById("username") as HTMLInputElement)?.value;
+    let password = (document.getElementById("password") as HTMLInputElement)?.value;
+    console.log(username);
+};
