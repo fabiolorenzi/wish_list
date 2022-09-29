becb9c6622a4826943ded48849e37abea3006279
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-09-29 13:11:12 +0100

    developed code to make the transition effect in the user page

--- scripts/typescript/user/user.ts
+++ scripts/typescript/user/user.ts
@@ -3,3 +3,19 @@ function login() {
     let password = (document.getElementById("password") as HTMLInputElement)?.value;
     console.log(username);
 };
+
+function moveRight(): void {
+    let bar = document.getElementById("user_bar");
+    if (bar.classList.contains("user_barRight")) {
+        bar.classList.remove("user_barRight");
+    };
+    bar.classList.add("user_barLeft");
+};
+
+function moveLeft(): void {
+    let bar = document.getElementById("user_bar");
+    if (bar.classList.contains("user_barLeft")) {
+        bar.classList.remove("user_barLeft");
+    };
+    bar.classList.add("user_barRight");
+};
