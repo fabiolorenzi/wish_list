526f94b280d48946283f828ad65d2fe813cc34a6
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-10-01 14:41:25 +0100

    developed route to update product

--- routes/products.py
+++ routes/products.py
@@ -11,7 +11,7 @@ def renderPage():
     return render_template("pages/wish-list/wish-list.html")
 
 @products_app.route("/api/products", methods = ["POST"])
-def createPage():
+def createProduct():
     name = request.json["name"]
     category = request.json["category"]
     created_at = datetime.now()
@@ -29,3 +29,34 @@ def createPage():
     db.session.add(newProduct)
     db.session.commit()
     return models.product.product_schema.jsonify(newProduct)
+
+@products_app.route("/api/products/<id>", methods = ["PUT"])
+def updateProduct(id):
+    name = request.json["name"]
+    category = request.json["category"]
+    updated_at = datetime.now()
+    country = request.json["country"]
+    materials = request.json["materials"]
+    image_url = request.json["image_url"]
+    taste = request.json["taste"]
+    aroma = request.json["aroma"]
+    weight = request.json["weight"]
+    quantity = request.json["quantity"]
+    price = request.json["price"]
+
+    productTarget = models.product.Products.query.get(id)
+
+    productTarget.name = name or productTarget.name
+    productTarget.category = category or productTarget.category
+    productTarget.updated_at = updated_at
+    productTarget.country = country or productTarget.country
+    productTarget.materials = materials or productTarget.materials
+    productTarget.image_url = image_url or productTarget.image_url
+    productTarget.taste = taste or productTarget.taste
+    productTarget.aroma = aroma or productTarget.aroma
+    productTarget.weight = weight or productTarget.weight
+    productTarget.quantity = quantity or productTarget.quantity
+    productTarget.price = price or productTarget.price
+
+    db.session.commit()
+    return models.product.product_schema.jsonify(productTarget)
