105536100b5eff993d4f00e4954ea6f83a8a8c58
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-10-03 18:28:59 +0100

    made menus dynamically change destination

--- scripts/index.ts
+++ scripts/index.ts
@@ -25,6 +25,28 @@ function permissionCheck(): void {
     .catch(err => console.log(err));
 };
 
+function setLinks(): void {
+    let userId = localStorage.getItem("user_id");
+    let dashboardLink = document.getElementById("dashboard_link");
+    let wishListLink = document.getElementById("wish-list_link");
+    let reviewsLink = document.getElementById("reviews_link");
+    let accountLink = document.getElementById("account_link");
+    let dashboardLinkMobile = document.getElementById("dashboard_linkMobile");
+    let wishListLinkMobile = document.getElementById("wish-list_linkMobile");
+    let reviewsLinkMobile = document.getElementById("reviews_linkMobile");
+    let accountLinkMobile = document.getElementById("account_linkMobile");
+
+    dashboardLink.setAttribute("href", `/dashboard?user=${userId}`);
+    wishListLink.setAttribute("href", `/wish-list?user=${userId}`);
+    reviewsLink.setAttribute("href", `/reviews?user=${userId}`);
+    accountLink.setAttribute("href", `/account?user=${userId}`);
+    dashboardLinkMobile.setAttribute("href", `/dashboard?user=${userId}`);
+    wishListLinkMobile.setAttribute("href", `/wish-list?user=${userId}`);
+    reviewsLinkMobile.setAttribute("href", `/reviews?user=${userId}`);
+    accountLinkMobile.setAttribute("href", `/account?user=${userId}`);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     permissionCheck();
+    setLinks();
 });
