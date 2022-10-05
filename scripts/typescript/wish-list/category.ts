15166989a11d6bf3ea512b5346b5821733cc8879
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-10-05 15:21:04 +0100

    developed code to open and close the category folder in wish-list page

--- scripts/typescript/wish-list/category.ts
+++ scripts/typescript/wish-list/category.ts
@@ -12,3 +12,17 @@ function deleteCategory(id: number): void {
     })
     .catch(err => console.log(err));
 };
+
+let categoryOpen = false;
+
+function openCloseCategory(targetId: string): void {
+    let category = document.getElementById(targetId);
+    categoryOpen = !categoryOpen;
+    if (categoryOpen) {
+        category.classList.add("category_bodyOpen");
+        category.classList.remove("category_bodyClosed");
+    } else {
+        category.classList.add("category_bodyClosed");
+        category.classList.remove("category_bodyOpen");
+    };
+};
