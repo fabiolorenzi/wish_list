5624a99a260397362dc9dc83d03cf8ec7b9584f7
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-10-05 18:46:08 +0100

    improved code for the clear cache system

--- tools/clearCache.py
+++ tools/clearCache.py
@@ -5,6 +5,10 @@ shutil.rmtree(os.path.join("__pycache__"))
 shutil.rmtree(os.path.join("models/__pycache__"))
 shutil.rmtree(os.path.join("routes/__pycache__"))
 shutil.rmtree(os.path.join("tools/__pycache__"))
+shutil.rmtree(os.path.join("static/scss"))
+shutil.rmtree(os.path.join("static/typescript"))
+os.remove("static/index.js")
+os.remove("static/index.js.map")
 
 if os.path.isdir("__migration__"):
     shutil.rmtree(os.path.join("migrations/__pycache__"))
