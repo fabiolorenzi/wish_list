98913b3ae9b9dc331e4ea77b6f4b74897800b33e
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-10-06 16:02:27 +0100

    developed code to copy review in wish list

--- scripts/typescript/reviews/review.ts
+++ scripts/typescript/reviews/review.ts
@@ -12,6 +12,44 @@ function openCloseReview(targetId: string): void {
     };
 };
 
+function copyReviewToWishList(id: number): void {
+    let name = (document.getElementById("reviewName" + id) as HTMLInputElement)?.value;
+    let category = (document.getElementById("reviewCategory" + id) as HTMLInputElement)?.value;
+    let country = (document.getElementById("reviewCountry" + id) as HTMLInputElement)?.value;
+    let materials = (document.getElementById("reviewMaterials" + id) as HTMLInputElement)?.value;
+    let image_url = (document.getElementById("reviewImageUrl" + id) as HTMLInputElement)?.value;
+    let taste = (document.getElementById("reviewTaste" + id) as HTMLInputElement)?.value;
+    let aroma = (document.getElementById("reviewAroma" + id) as HTMLInputElement)?.value;
+    let weight = (document.getElementById("reviewWeight" + id) as HTMLInputElement)?.value;
+    let price = (document.getElementById("reviewPrice" + id) as HTMLInputElement)?.value;
+
+    fetch(`/api/products`, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            category: category,
+            country: country,
+            materials: materials,
+            image_url: image_url,
+            taste: taste,
+            aroma: aroma,
+            weight: weight,
+            quantity: 0,
+            price: price,
+            created_by: localStorage.getItem("user_id")
+        })
+    })
+    .then(() => {
+        alert("Review copied successfully");
+        window.location.reload();
+    })
+    .catch(err => console.log(err));
+};
+
 function updateReview(id: number): void {
     let name = (document.getElementById("reviewName" + id) as HTMLInputElement)?.value;
     let category = (document.getElementById("reviewCategory" + id) as HTMLInputElement)?.value;
