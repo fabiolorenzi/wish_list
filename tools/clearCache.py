7a44f31c7803cbfcf514614070570a96226ccbe3
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-10-06 19:25:29 +0100

    added comment to clearCache tool

--- tools/clearCache.py
+++ tools/clearCache.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+# this file is to clean the cache and also the built js and css
+
 shutil.rmtree(os.path.join("__pycache__"))
 shutil.rmtree(os.path.join("models/__pycache__"))
 shutil.rmtree(os.path.join("routes/__pycache__"))
